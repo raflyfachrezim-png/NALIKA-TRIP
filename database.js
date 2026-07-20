64ff8b2a89aa7dd25d77d0a367a227744563642e
raflyfachrezim-png <raflyfachrezim@gmail.com>
2026-07-20 20:21:57 +0700

    Update image paths in database.js

--- database.js
+++ database.js
@@ -11,19 +11,19 @@ const NALIKA_DATABASE = {
         title: "Tentang Nalika",
         desc1: "Nalika Trip menghadirkan pengalaman wisata alam dan kegiatan edukatif yang seru, aman, dan penuh makna. Setiap perjalanan dirancang untuk menciptakan momen kebersamaan yang tak terlupakan.",
         desc2: "Didukung dokumentasi foto berkualitas dan video sinematik, setiap cerita akan tetap hidup bahkan setelah perjalanan berakhir.",
-        image: "images/1.jpg"
+        image: "images/galeri 10.jpg"
     },
 
-    // Foto Galeri (8 foto dari folder images)
+    // Foto Galeri (Cukup ganti/tambah foto di sini aja, Pee!)
     gallery: [
-        { image: "galeri 1.jpg" },
-        { image: "galeri 2.jpg" },
-        { image: "galeri 3.jpg" },
-        { image: "galeri 4.jpg" },
-        { image: "galeri 5.jpg" },
-        { image: "galeri 6.jpg" },
-        { image: "galeri 7.jpg" },
-        { image: "galeri 8.jpg" },
+        { image: "images/galeri 1.jpg" },
+        { image: "images/galeri 2.jpg" },
+        { image: "images/galeri 3.jpg" },
+        { image: "images/galeri 4.jpg" },
+        { image: "images/galeri 5.jpg" },
+        { image: "images/galeri 6.jpg" },
+        { image: "images/galeri 7.jpg" },
+        { image: "images/galeri 8.jpg" }
     ],
 
     // Detail Pilihan Trip
@@ -35,7 +35,7 @@ const NALIKA_DATABASE = {
             meetingPoint: "Indomaret Terminal Cidahu",
             schedule: "Weekend|Weekday",
             desc: "Kawah aktif sisa letusan Gunung Salak yang terakhir terjadi pada tahun 1938. Terletak di kawasan Taman Nasional Gunung Halimun Salak (TNGHS) di perbatasan Bogor dan Sukabumi, kawah seluas 2 hektare ini menjadi destinasi hiking populer karena keunikan uap panas dan aliran air belerangnya.",
-            image: "images/1.jpg",
+            image: "images/galeri 5.jpg",
             waLink: "https://wa.me/628212240439?text=Halo%20Nalika%20Trip,%20saya%20mau%20register%20untuk%20Trip%20Kawah%20Ratu",
             includes: ["Tiket Masuk", "Simaksi", "Asuransi", "Dokumentasi", "P3K Standar", "Makan Siang", "Air Mineral", "Parkir Motor", "Tour Guide"],
             excludes: ["Peralatan Pribadi", "Obat Pribadi", "Pakaian Ganti", "Snack", "Jas Hujan", "Parkir Mobil", "Ojek Mepo - BC"]
@@ -47,7 +47,7 @@ const NALIKA_DATABASE = {
             meetingPoint: "Sukabumi (Exit Tol Parungkuda), Bogor (Ciawi), Jakarta (Cawang UKI)",
             schedule: "Weekend|Weekday",
             desc: "Gunung Papandayan adalah gunung api aktif setinggi 2.665 mdpl di Cisurupan, Garut, Jawa Barat. Terkenal ramah untuk pemula, daya tarik utamanya meliputi Kawah Mas yang eksotis, area camping Pondok Salada, hamparan bunga edelweiss di Tegal Alun, serta lanskap dramatis Hutan Mati.",
-            image: "images/2.jpg",
+            image: "images/galeri 14.jpg",
             waLink: "https://wa.me/628212240439?text=Halo%20Nalika%20Trip,%20saya%20mau%20register%20untuk%20Trip%20Gunung%20Papandayan",
             includes: ["Transportasi PP", "Tiket / Simaksi", "Rumah Singgah", "Makan 1x", "Minuman", "Dokumentasi", "P3K Standar", "Tour Guide"],
             excludes: ["Obat Pribadi", "Surat Sehat", "Peralatan Pribadi", "Baju Ganti", "Snack / Makanan Tambahan", "Minum Pribadi", "Jas Hujan"]
